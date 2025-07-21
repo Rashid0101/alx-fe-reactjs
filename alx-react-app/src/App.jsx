ce6cfa7d98c2ed64fa4445693d24ab956380e3cb
Rashid <127450565+Rashid0101@users.noreply.github.com>
2025-07-21 06:36:44 +0300

    aaaqgy

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,8 @@
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
+
 import "./App.css";
 
 function App() {
@@ -9,9 +11,9 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </div>
   );
 }
 
 export default App;
-// adsa
