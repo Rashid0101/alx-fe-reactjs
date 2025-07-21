2f1cb4a4e5e710adc147e525a7e5818fa8e0814b
Rashid <127450565+Rashid0101@users.noreply.github.com>
2025-07-21 06:29:09 +0300

    finshd

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,16 @@
-import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
+
 export default App;
